9459dd274f823b75f7af7d1ef6acf5d7052351dc
Akshat Prakash <akshatprakash@Akshats-MacBook-Pro.local>
2020-03-17 08:30:19 -0400

    Pushed code to repo

---
+++ app/src/main/java/com/example/notificationexample/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.notificationexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startServiceButton = (Button)findViewById(R.id.start_foreground_service_button);
+        startServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForegroundService.class);
+                intent.setAction(ForegroundService.ACTION_START_FOREGROUND_SERVICE);
+                startService(intent);
+            }
+        });
+
+        Button stopServiceButton = (Button)findViewById(R.id.stop_foreground_service_button);
+        stopServiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForegroundService.class);
+                intent.setAction(ForegroundService.ACTION_STOP_FOREGROUND_SERVICE);
+                startService(intent);
+            }
+        });
+    }
+}
